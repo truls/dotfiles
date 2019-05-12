853998f0a427008ea0bef81a1a850f5d425df228
Truls Asheim <truls@asheim.dk>
2019-05-12 16:51:26 +0200

    Add more safe variables

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values (quote ((haskell-stylish-on-save)))))
+ '(safe-local-variable-values
+   (quote
+    ((haskell-stylish-on-save)
+     (eval bibtex-set-dialect
+           (quote biblatex))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
