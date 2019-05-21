75457c359887d4d6cebfa39b63d406fbd834738a
Truls Asheim <truls@asheim.dk>
2019-05-21 13:18:35 +0200

    Updated custom

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,6 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (forge org-ref hydra znc yasnippet-snippets yaml-mode web-mode visual-regexp-steroids visual-fill-column use-package-hydra typescript-mode ssass-mode solidity-mode solidity-flycheck smex smart-shift org-gcal org-bullets nlinum mutt-mode magit magic-latex-buffer lsp-ui lsp-haskell langtool js2-mode ivy-yasnippet ivy-xref highlight-indentation helm-rg helm-projectile google-this flyspell-correct-popup flycheck ess diminish counsel-projectile company-solidity company-lsp color-theme-modern ccls auto-package-update auto-compile auctex)))
  '(safe-local-variable-values
    (quote
     ((haskell-stylish-on-save)
