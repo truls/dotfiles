109acc1c01ac905e59eef8f2d4f9cc6b1cb87809
Truls Asheim <truls@asheim.dk>
2019-06-03 12:39:16 +0200

    state

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,12 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   (quote
+    ("~/Nextcloud/bibliography/notes.org" "~/Nextcloud/org/todo.org" "~/Nextcloud/org/gcal.org")))
  '(package-selected-packages
    (quote
-    (forge org-ref hydra znc yasnippet-snippets yaml-mode web-mode visual-regexp-steroids visual-fill-column use-package-hydra typescript-mode ssass-mode solidity-mode solidity-flycheck smex smart-shift org-gcal org-bullets nlinum mutt-mode magit magic-latex-buffer lsp-ui lsp-haskell langtool js2-mode ivy-yasnippet ivy-xref highlight-indentation helm-rg helm-projectile google-this flyspell-correct-popup flycheck ess diminish counsel-projectile company-solidity company-lsp color-theme-modern ccls auto-package-update auto-compile auctex)))
+    (ace-window switch-window dockerfile-mode dockerfile magit ssass-mode forge org-ref hydra znc yasnippet-snippets yaml-mode web-mode visual-regexp-steroids visual-fill-column use-package-hydra typescript-mode solidity-mode solidity-flycheck smex smart-shift org-gcal org-bullets nlinum mutt-mode magic-latex-buffer lsp-ui lsp-haskell langtool js2-mode ivy-yasnippet ivy-xref highlight-indentation helm-rg helm-projectile google-this flyspell-correct-popup flycheck ess diminish counsel-projectile company-solidity company-lsp color-theme-modern ccls auto-package-update auto-compile auctex)))
  '(safe-local-variable-values
    (quote
-    ((haskell-stylish-on-save)
+    ((lexical-scoping . t)
+     (nameless-current-name . "biblio")
+     (haskell-stylish-on-save)
      (eval bibtex-set-dialect
            (quote biblatex))))))
 (custom-set-faces
