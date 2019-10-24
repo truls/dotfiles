ce752a19f80803620b6c63fcf4388d5ebd83436c
Truls Asheim <truls@asheim.dk>
2019-10-24 14:24:48 +0200

    Make C-backspase delete, rather than kill, a word

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1407,5 +1407,27 @@ point reaches the beginning or end of the buffer, stop there."
 (global-set-key (kbd "C-S-c") #'comment-duplicate-line)
 (global-set-key (kbd "C-S-d") #'duplicate-line)
 
+;;
+;; Delete word on C-backspace
+;;
+
+;; From: https://www.emacswiki.org/emacs/BackwardDeleteWord
+(defun delete-word (arg)
+  "Delete characters forward until encountering the end of a word.
+With argument, do this that many times."
+  (interactive "p")
+  (if (use-region-p)
+      (delete-region (region-beginning) (region-end))
+    (delete-region (point) (progn (forward-word arg) (point)))))
+
+(defun backward-delete-word (arg)
+  "Delete characters backward until encountering the end of a word.
+With argument, do this that many times."
+  (interactive "p")
+  (delete-word (- arg)))
+
+(global-set-key (read-kbd-macro "<M-DEL>") 'backward-delete-word)
+(global-set-key (read-kbd-macro "<C-Backspace>") 'backward-delete-word)
+
 (provide 'init)
 ;;; init.el ends here
