5f79225e572999aae0c699b67a028ae85cc5e6aa
Truls Asheim <truls@asheim.dk>
2024-12-08 18:09:37 +0100

    Sync custom

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -4,138 +4,133 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(connection-local-criteria-alist
-   '(((:application tramp)
-      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+   '(((:application tramp :protocol "kubernetes")
+      tramp-kubernetes-connection-local-default-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile
+      tramp-connection-local-default-shell-profile)))
  '(connection-local-profile-alist
-   '((tramp-connection-local-darwin-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format
-       (pid . number)
-       (euid . number)
-       (user . string)
-       (egid . number)
-       (comm . 52)
-       (state . 5)
-       (ppid . number)
-       (pgrp . number)
-       (sess . number)
-       (ttname . string)
-       (tpgid . number)
-       (minflt . number)
-       (majflt . number)
-       (time . tramp-ps-time)
-       (pri . number)
-       (nice . number)
-       (vsize . number)
-       (rss . number)
-       (etime . tramp-ps-time)
-       (pcpu . number)
-       (pmem . number)
-       (args)))
+   '((tramp-kubernetes-connection-local-default-profile
+      (tramp-config-check . tramp-kubernetes--current-context-data)
+      (tramp-extra-expand-args 97
+                               (tramp-kubernetes--container
+                                (car tramp-current-connection))
+                               104
+                               (tramp-kubernetes--pod
+                                (car tramp-current-connection))
+                               120
+                               (tramp-kubernetes--context-namespace
+                                (car tramp-current-connection))))
+     (tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o"
+                                        "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "-o" "state=abcde" "-o"
+                                        "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format (pid . number)
+                                          (euid . number)
+                                          (user . string)
+                                          (egid . number) (comm . 52)
+                                          (state . 5) (ppid . number)
+                                          (pgrp . number)
+                                          (sess . number)
+                                          (ttname . string)
+                                          (tpgid . number)
+                                          (minflt . number)
+                                          (majflt . number)
+                                          (time . tramp-ps-time)
+                                          (pri . number)
+                                          (nice . number)
+                                          (vsize . number)
+                                          (rss . number)
+                                          (etime . tramp-ps-time)
+                                          (pcpu . number)
+                                          (pmem . number) (args)))
      (tramp-connection-local-busybox-ps-profile
-      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
-      (tramp-process-attributes-ps-format
-       (pid . number)
-       (user . string)
-       (group . string)
-       (comm . 52)
-       (state . 5)
-       (ppid . number)
-       (pgrp . number)
-       (ttname . string)
-       (time . tramp-ps-time)
-       (nice . number)
-       (etime . tramp-ps-time)
-       (args)))
+      (tramp-process-attributes-ps-args "-o"
+                                        "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "-o" "stat=abcde" "-o"
+                                        "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format (pid . number)
+                                          (user . string)
+                                          (group . string) (comm . 52)
+                                          (state . 5) (ppid . number)
+                                          (pgrp . number)
+                                          (ttname . string)
+                                          (time . tramp-ps-time)
+                                          (nice . number)
+                                          (etime . tramp-ps-time)
+                                          (args)))
      (tramp-connection-local-bsd-ps-profile
-      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
-      (tramp-process-attributes-ps-format
-       (pid . number)
-       (euid . number)
-       (user . string)
-       (egid . number)
-       (group . string)
-       (comm . 52)
-       (state . string)
-       (ppid . number)
-       (pgrp . number)
-       (sess . number)
-       (ttname . string)
-       (tpgid . number)
-       (minflt . number)
-       (majflt . number)
-       (time . tramp-ps-time)
-       (pri . number)
-       (nice . number)
-       (vsize . number)
-       (rss . number)
-       (etime . number)
-       (pcpu . number)
-       (pmem . number)
-       (args)))
+      (tramp-process-attributes-ps-args "-acxww" "-o"
+                                        "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                        "-o"
+                                        "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format (pid . number)
+                                          (euid . number)
+                                          (user . string)
+                                          (egid . number)
+                                          (group . string) (comm . 52)
+                                          (state . string)
+                                          (ppid . number)
+                                          (pgrp . number)
+                                          (sess . number)
+                                          (ttname . string)
+                                          (tpgid . number)
+                                          (minflt . number)
+                                          (majflt . number)
+                                          (time . tramp-ps-time)
+                                          (pri . number)
+                                          (nice . number)
+                                          (vsize . number)
+                                          (rss . number)
+                                          (etime . number)
+                                          (pcpu . number)
+                                          (pmem . number) (args)))
      (tramp-connection-local-default-shell-profile
-      (shell-file-name . "/bin/sh")
-      (shell-command-switch . "-c"))
+      (shell-file-name . "/bin/sh") (shell-command-switch . "-c"))
      (tramp-connection-local-default-system-profile
-      (path-separator . ":")
-      (null-device . "/dev/null"))))
+      (path-separator . ":") (null-device . "/dev/null"))))
  '(display-line-numbers-width 1)
  '(org-agenda-files
-   '("~/Nextcloud/bibliography/notes.org" "~/Nextcloud/org/todo.org" "~/Nextcloud/org/gcal.org"))
+   '("~/Nextcloud/bibliography/notes.org" "~/Nextcloud/org/todo.org"
+     "~/Nextcloud/org/gcal.org"))
  '(safe-local-variable-values
-   '((TeX-master . "main")
-     (TeX-master . t)
-     (eval add-hook 'yaml-mode-hook
-           '(lambda nil
-              (ansible 1)
-              nil t))
-     (blacken-skip-string-normalization . t)
-     (blacken-allow-py36 . t)
-     (blacken-line-length . 100)
-     (TeX-master \` `main)
-     (TeX-command-extra-options . "-shell-escape")
-     (TeX-master . main)
+   '((TeX-engine . xetex) (TeX-master . "main") (TeX-master . t)
+     (eval add-hook 'yaml-mode-hook '(lambda nil (ansible 1) nil t))
+     (blacken-skip-string-normalization . t) (blacken-allow-py36 . t)
+     (blacken-line-length . 100) (TeX-master \` `main)
+     (TeX-command-extra-options . "-shell-escape") (TeX-master . main)
      (eval when
-           (and
-            (buffer-file-name)
-            (not
-             (file-directory-p
-              (buffer-file-name)))
-            (string-match-p "^[^.]"
-                            (buffer-file-name)))
-           (unless
-               (featurep 'package-build)
-             (let
-                 ((load-path
-                   (cons "../package-build" load-path)))
+           (and (buffer-file-name)
+                (not (file-directory-p (buffer-file-name)))
+                (string-match-p "^[^.]" (buffer-file-name)))
+           (unless (featurep 'package-build)
+             (let ((load-path (cons "../package-build" load-path)))
                (require 'package-build)))
-           (unless
-               (derived-mode-p 'emacs-lisp-mode)
+           (unless (derived-mode-p 'emacs-lisp-mode)
              (emacs-lisp-mode))
            (package-build-minor-mode)
            (setq-local flycheck-checkers nil)
-           (set
-            (make-local-variable 'package-build-working-dir)
-            (expand-file-name "../working/"))
-           (set
-            (make-local-variable 'package-build-archive-dir)
-            (expand-file-name "../packages/"))
-           (set
-            (make-local-variable 'package-build-recipes-dir)
-            default-directory))
-     (lsp-python-ms-extra-paths "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
-     (lsp-python-ms-extra-paths vector "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
+           (set (make-local-variable 'package-build-working-dir)
+                (expand-file-name "../working/"))
+           (set (make-local-variable 'package-build-archive-dir)
+                (expand-file-name "../packages/"))
+           (set (make-local-variable 'package-build-recipes-dir)
+                default-directory))
+     (lsp-python-ms-extra-paths
+      "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
+     (lsp-python-ms-extra-paths vector
+                                "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
      (lsp-python-ms-extra-paths quote
                                 ("/home/truls/uni/teaching/algdat-tdt4120/inginious-installed"))
-     (lsp-python-ms-extra-paths . "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
-     (lsp-python-ms-dir . "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
+     (lsp-python-ms-extra-paths
+      . "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
+     (lsp-python-ms-dir
+      . "/home/truls/uni/teaching/algdat-tdt4120/inginious-installed")
      (TeX-command-extra-options . "-enable-write18")
-     (sh-indent-for-case-alt . +)
-     (sh-indent-for-case-label . 0)
-     (lexical-scoping . t)
-     (nameless-current-name . "biblio")
-     (haskell-stylish-on-save)
-     (eval bibtex-set-dialect 'biblatex)))
+     (sh-indent-for-case-alt . +) (sh-indent-for-case-label . 0)
+     (lexical-scoping . t) (nameless-current-name . "biblio")
+     (haskell-stylish-on-save) (eval bibtex-set-dialect 'biblatex)))
  '(warning-suppress-log-types '((lsp-mode) (comp) (:warning))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
