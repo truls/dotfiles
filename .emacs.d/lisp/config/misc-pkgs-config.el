cdf5eb2736b45b514df9e2794e074a497840694e
Truls Asheim <truls@asheim.dk>
2025-01-29 11:35:47 +0100

    Add fzf frontend

--- .emacs.d/lisp/config/misc-pkgs-config.el
+++ .emacs.d/lisp/config/misc-pkgs-config.el
@@ -235,6 +235,22 @@
 (use-package helm-ag
   :straight t)
 
+(use-package fzf
+  :straight t
+  :bind
+    ;; Don't forget to set keybinds!
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        ;; fzf/grep-command "rg --no-heading -nH"
+        fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
+
 (use-package pdf-tools
   :straight t
   :preface
