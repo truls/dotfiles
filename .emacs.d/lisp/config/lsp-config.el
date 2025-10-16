26e57fffe078fe367c13f5c6ac7d1ee98132e411
Truls Asheim <truls@asheim.dk>
2025-10-16 13:13:17 +0200

    remove obsolete python lsp config

--- .emacs.d/lisp/config/lsp-config.el
+++ .emacs.d/lisp/config/lsp-config.el
@@ -122,46 +122,6 @@
 (use-package ruff-format
   :straight t)
 
-(use-package lsp-pyright
-  :straight t)
-
-(use-package pet
-  :ensure-system-package (dasel sqlite3)
-  :straight t
-  :config
-  (add-hook 'python-base-mode-hook
-            (lambda ()
-              (pet-mode -10)
-              (setq-local python-shell-interpreter (pet-executable-find "python")
-                          python-shell-virtualenv-root (pet-virtualenv-root))
-
-              (pet-flycheck-setup)
-              (flycheck-mode)
-
-              (setq-local lsp-jedi-executable-command
-                          (pet-executable-find "jedi-language-server"))
-
-              (setq-local lsp-pyright-python-executable-cmd python-shell-interpreter
-                          lsp-pyright-venv-path python-shell-virtualenv-root)
-
-              (lsp)
-
-              (setq-local dap-python-executable python-shell-interpreter)
-
-              (setq-local python-pytest-executable (pet-executable-find "pytest"))
-
-              (when-let ((ruff-executable (pet-executable-find "ruff")))
-                (setq-local ruff-format-command ruff-executable)
-                (ruff-format-on-save-mode))
-
-              (when-let ((black-executable (pet-executable-find "black")))
-                (setq-local python-black-command black-executable)
-                (python-black-on-save-mode))
-
-              (when-let ((isort-executable (pet-executable-find "isort")))
-                (setq-local python-isort-command isort-executable)
-                (python-isort-on-save-mode)))))
-
 (use-package lsp-haskell
   :straight t)
 
