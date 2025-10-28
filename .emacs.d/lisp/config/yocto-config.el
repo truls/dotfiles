f06d4b8814656872f9b205c0144efea5d9bc23a3
Truls Asheim <truls@asheim.dk>
2025-10-28 10:54:09 +0100

    switch to bitbake-ts-mode and inhibit problematic minor modes

--- .emacs.d/lisp/config/yocto-config.el
+++ .emacs.d/lisp/config/yocto-config.el
@@ -1,9 +1,10 @@
 ;; BitBake-mode
-(use-package bitbake
-  :straight t
-  :config
-  (setq auto-mode-alist
-        (delete `(,bitbake-mode-file-regex . bitbake-mode) auto-mode-alist)))
+(use-package bitbake-ts-mode
+  :init
+  (add-to-list
+   'treesit-language-source-alist
+   '(bitbake "https://github.com/tree-sitter-grammars/tree-sitter-bitbake"))
+  :straight t)
 
 (flycheck-define-checker oelint-adv
   "A linter for BitBake recipes using oelint-adv."
@@ -28,26 +29,33 @@
       ('bitbake-mode (flycheck-select-checker 'oelint-adv))
       (_ (flycheck-mode -1))))) ;; Disable Flycheck if no checker is found
 
-(add-hook 'polymode-init-host-hook #'flycheck-mode)
-(add-hook 'polymode-init-inner-hook #'flycheck-mode)
-(add-hook 'after-change-major-mode-hook #'my/set-flycheck-checker-based-on-mode)
-(add-hook 'poly-mode-hook #'my/set-flycheck-checker-based-on-mode)
-(add-hook 'after-change-major-mode-hook #'my/set-flycheck-checker-based-on-mode)
+(defun my-bitbake-python-ts-mode-wrapper ()
+  "A wrapper around `python-ts-mode` that disables certain hooks when used inside poly-bitbake-mode."
+  (let ((python-ts-mode-hook
+         (remove 'ruff-format-on-save-mode
+                 (remove 'lsp-deferred python-ts-mode-hook)))
+        (python-mode-hook
+         (remove 'ruff-format-on-save-mode
+                 (remove 'lsp-deferred python-mode-hook)))
+        ;; Prevent after-change-major-mode-hook triggers from enabling LSP globally
+        (after-change-major-mode-hook
+         (cl-remove-if (lambda (fn)
+                         (or (eq fn #'lsp)
+                             (eq fn #'lsp-deferred)))
+                       after-change-major-mode-hook)))
+    (python-ts-mode)))
 
-;; TODO: Try to make this work in order to restrict tle flycheck
-;; checker to the innermost code region
-;;
-;;
-;; (defun my/flycheck-limit-to-inner-region ()
-;;   "Limit Flycheck to the current inner mode region in Polymode."
-;;   (when (bound-and-true-p polymode-mode)
-;;     (let ((span (pm-innermost-span)))
-;;       (when span
-;;         (setq-local flycheck-region-function
-;;                     (lambda (_start _end)
-;;                       (list (nth 1 span) (nth 2 span))))))))
+(defun my-bitbake-sh-mode-wrapper ()
+  "A wrapper around `python-ts-mode` that disables certain hooks when used inside poly-bitbake-mode."
+  (let ((sh-mode-hook (remove 'lsp-deferred sh-mode-hook))
+        ;; Prevent after-change-major-mode-hook triggers from enabling LSP globally
+        (after-change-major-mode-hook
+         (cl-remove-if (lambda (fn)
+                         (or (eq fn #'lsp)
+                             (eq fn #'lsp-deferred)))
+                       after-change-major-mode-hook)))
+    (sh-mode)))
 
-;; (add-hook 'polymode-switch-buffer-hook #'my/flycheck-limit-to-inner-region)
 
 (define-derived-mode bash-script-mode sh-mode "Bash"
   "A major mode for editing Bash scripts, ensuring `sh-mode` uses Bash."
@@ -60,46 +68,43 @@
   :straight t
   :config
 
-    (define-hostmode poly-bitbake-hostmode
-      :mode 'bitbake-mode)
+  (define-hostmode poly-bitbake-hostmode
+    :mode 'bitbake-ts-mode
+    :protect-font-lock t)
 
-    (define-innermode poly-bitbake-shell-innermode
-      :mode 'bash-script-mode
-      :head-matcher bitbake-shell-regex
-      :tail-matcher "^}"
-      :head-mode 'host
-      :tail-mode 'host)
+  (define-innermode poly-bitbake-shell-innermode
+    :mode #'my-bitbake-sh-mode-wrapper
+    :head-matcher bitbake-shell-regex
+    :tail-matcher "^}"
+    :head-mode 'host
+    :tail-mode 'host
+    :protect-font-lock t)
 
-    (define-innermode poly-bitbake-python-innermode
-      :mode 'python-mode
-      :head-matcher bitbake-python-regex
-      :tail-matcher "^}"
-      :head-mode 'host
-      :tail-mode 'host)
+  (define-innermode poly-bitbake-python-innermode
+    :mode #'my-bitbake-python-ts-mode-wrapper
+    :head-matcher bitbake-python-regex
+    :tail-matcher "^}"
+    :head-mode 'host
+    :tail-mode 'host
+    :protect-font-lock t)
 
-    (define-innermode poly-bitbake-python-def-innermode
-      :mode 'python-mode
-      :head-matcher bitbake-python-def-regex
-      :tail-matcher "\\(^[^[:space:]\n]\\)"
-      :head-mode 'host
-      :tail-mode 'host)
+  (define-innermode poly-bitbake-python-def-innermode
+    :mode #'my-bitbake-python-ts-mode-wrapper
+    :head-matcher bitbake-python-def-regex
+    :tail-matcher "^$"
+    :head-mode 'host
+    :tail-mode 'host
+    :protect-font-lock t)
 
-    (define-innermode poly-bitbake-python-inline-innermode
-      :mode 'python-mode
-      :head-matcher "${@"
-      :tail-matcher "}"
-      :head-mode 'host
-      :tail-mode 'host)
 
-    (define-polymode poly-bitbake-mode
-      :hostmode 'poly-bitbake-hostmode
-      :innermodes '(poly-bitbake-python-innermode
-                    poly-bitbake-python-inline-innermode
-                    poly-bitbake-python-def-innermode
-                    poly-bitbake-shell-innermode))
+  (define-polymode poly-bitbake-mode
+    :hostmode 'poly-bitbake-hostmode
+    :innermodes '(poly-bitbake-python-innermode
+                  poly-bitbake-python-def-innermode
+                  poly-bitbake-shell-innermode))
 
-    (setq auto-mode-alist
-          (delete `(,bitbake-mode-file-regex . bitbake-mode) auto-mode-alist))
-    (add-to-list 'auto-mode-alist `(,bitbake-mode-file-regex . poly-bitbake-mode)))
+  (setq auto-mode-alist
+        (delete `(,bitbake-mode-file-regex . bitbake-mode) auto-mode-alist))
+  (add-to-list 'auto-mode-alist `(,bitbake-mode-file-regex . poly-bitbake-mode)))
 
 (provide 'yocto-config)
